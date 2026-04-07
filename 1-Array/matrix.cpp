d9518684c4cc6a0584b35eef1c5851e4e673ce96
yubin <munu220@gmail.com>
2026-04-07 12:30:06 +0900

    add matrix skel

---
+++ 1-Array/matrix.cpp
@@ -0,0 +1,28 @@
+#include "Matrix.h"
+
+Matrix::Matrix(int max_terms) {
+}
+
+Matrix::~Matrix() {
+}
+
+void Matrix::addElement(int r, int c, double val) {
+}
+
+Matrix::Matrix(const Matrix& other) {
+}
+
+Matrix& Matrix::operator=(const Matrix& other) {
+}
+
+Matrix Matrix::operator+(const Matrix& other) {
+}
+
+Matrix Matrix::operator-(const Matrix& other) {
+}
+
+Matrix Matrix::operator*(const Matrix& other) {
+}
+
+Matrix Matrix::operator/(const Matrix& other) {
+}
