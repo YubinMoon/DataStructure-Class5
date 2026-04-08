696b382c65a24e521f0265315399da0e91955bb9
yubin <munu220@gmail.com>
2026-04-08 22:56:53 +0900

    code format

--- 1-Array/DenseMatrix.h
+++ 1-Array/DenseMatrix.h
@@ -1,26 +1,27 @@
 #ifndef DENSE_MATRIX_H
 #define DENSE_MATRIX_H
 
-class DenseMatrix {
+class DenseMatrix
+{
 private:
     int rows;
     int cols;
-    double** data;
+    double **data;
 
 public:
     DenseMatrix(int r, int c);
-    DenseMatrix(const DenseMatrix& other);
+    DenseMatrix(const DenseMatrix &other);
     ~DenseMatrix();
 
-    DenseMatrix& operator=(const DenseMatrix& other);
+    DenseMatrix &operator=(const DenseMatrix &other);
 
     void setValue(int r, int c, double val);
     double getValue(int r, int c) const;
 
-    DenseMatrix operator+(const DenseMatrix& other) const;
-    DenseMatrix operator-(const DenseMatrix& other) const;
-    DenseMatrix operator*(const DenseMatrix& other) const;
-    DenseMatrix operator/(const DenseMatrix& other) const;
+    DenseMatrix operator+(const DenseMatrix &other) const;
+    DenseMatrix operator-(const DenseMatrix &other) const;
+    DenseMatrix operator*(const DenseMatrix &other) const;
+    DenseMatrix operator/(const DenseMatrix &other) const;
 
     DenseMatrix transpose() const;
     void print() const;
