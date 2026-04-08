589368a3e5196fc2a1998abc279d3624b98b911f
yubin <munu220@gmail.com>
2026-04-08 23:36:42 +0900

    code format

--- 1-Array/SparseMatrix.h
+++ 1-Array/SparseMatrix.h
@@ -3,30 +3,32 @@
 
 #include <algorithm>
 
-struct Element {
+struct Element
+{
     int row;
     int col;
     double data;
 };
 
-class SparseMatrix {
+class SparseMatrix
+{
 private:
     int capacity;
-    Element* elements;
+    Element *elements;
 
 public:
     SparseMatrix(int r, int c, int cap = 10);
-    SparseMatrix(const SparseMatrix& other);
+    SparseMatrix(const SparseMatrix &other);
     ~SparseMatrix();
 
-    SparseMatrix& operator=(const SparseMatrix& other);
+    SparseMatrix &operator=(const SparseMatrix &other);
 
     void addElement(int r, int c, double val);
 
-    SparseMatrix operator+(const SparseMatrix& other) const;
-    SparseMatrix operator-(const SparseMatrix& other) const;
-    SparseMatrix operator*(const SparseMatrix& other) const;
-    SparseMatrix operator/(const SparseMatrix& other) const;
+    SparseMatrix operator+(const SparseMatrix &other) const;
+    SparseMatrix operator-(const SparseMatrix &other) const;
+    SparseMatrix operator*(const SparseMatrix &other) const;
+    SparseMatrix operator/(const SparseMatrix &other) const;
 
     SparseMatrix transpose() const;
     void print() const;
