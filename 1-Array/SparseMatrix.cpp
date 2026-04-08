947b2e5bc88a2f4712ec45ef9d8db24def4e1b8d
yubin <munu220@gmail.com>
2026-04-08 21:57:03 +0900

    테스트 추가

--- 1-Array/SparseMatrix.cpp
+++ 1-Array/SparseMatrix.cpp
@@ -1,45 +1,58 @@
 #include "SparseMatrix.h"
 #include <iostream>
+#include <algorithm>
 
-SparseMatrix::SparseMatrix(int r, int c, int cap) : capacity(cap) {
-    if (capacity < 1) capacity = 10;
+SparseMatrix::SparseMatrix(int r, int c, int cap) : capacity(cap)
+{
+    if (capacity < 1)
+        capacity = 10;
     elements = new Element[capacity];
     elements[0].row = r;
     elements[0].col = c;
     elements[0].data = 0;
 }
 
-SparseMatrix::SparseMatrix(const SparseMatrix& other) : capacity(other.capacity) {
+SparseMatrix::SparseMatrix(const SparseMatrix &other) : capacity(other.capacity)
+{
     elements = new Element[capacity];
     int terms = (int)other.elements[0].data;
-    for (int i = 0; i <= terms; ++i) {
+    for (int i = 0; i <= terms; ++i)
+    {
         elements[i] = other.elements[i];
     }
 }
 
-SparseMatrix::~SparseMatrix() {
+SparseMatrix::~SparseMatrix()
+{
     delete[] elements;
 }
 
-SparseMatrix& SparseMatrix::operator=(const SparseMatrix& other) {
-    if (this == &other) return *this;
+SparseMatrix &SparseMatrix::operator=(const SparseMatrix &other)
+{
+    if (this == &other)
+        return *this;
     delete[] elements;
     capacity = other.capacity;
     elements = new Element[capacity];
     int terms = (int)other.elements[0].data;
-    for (int i = 0; i <= terms; ++i) {
+    for (int i = 0; i <= terms; ++i)
+    {
         elements[i] = other.elements[i];
     }
     return *this;
 }
 
-void SparseMatrix::addElement(int r, int c, double val) {
-    if (val == 0.0) return;
+void SparseMatrix::addElement(int r, int c, double val)
+{
+    if (val == 0.0)
+        return;
     int terms = (int)elements[0].data;
-    if (terms + 1 == capacity) {
+    if (terms + 1 >= capacity)
+    {
         capacity *= 2;
-        Element* newElements = new Element[capacity];
-        for (int i = 0; i <= terms; ++i) {
+        Element *newElements = new Element[capacity];
+        for (int i = 0; i <= terms; ++i)
+        {
             newElements[i] = elements[i];
         }
         delete[] elements;
@@ -52,27 +65,15 @@ void SparseMatrix::addElement(int r, int c, double val) {
     elements[0].data = (double)terms;
 }
 
-void SparseMatrix::quickSort(int low, int high) {
-    if (low >= high) return;
-    Element pivot = elements[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (elements[j].row < pivot.row || (elements[j].row == pivot.row && elements[j].col < pivot.col)) {
-            i++;
-            Element temp = elements[i];
-            elements[i] = elements[j];
-            elements[j] = temp;
-        }
-    }
-    Element temp = elements[i + 1];
-    elements[i + 1] = elements[high];
-    elements[high] = temp;
-    int p = i + 1;
-    quickSort(low, p - 1);
-    quickSort(p + 1, high);
+static bool compareElements(const Element &a, const Element &b)
+{
+    if (a.row != b.row)
+        return a.row < b.row;
+    return a.col < b.col;
 }
 
-SparseMatrix SparseMatrix::operator+(const SparseMatrix& other) const {
+SparseMatrix SparseMatrix::operator+(const SparseMatrix &other) const
+{
     int rows = elements[0].row;
     int cols = elements[0].col;
     int aTerms = (int)elements[0].data;
@@ -80,31 +81,43 @@ SparseMatrix SparseMatrix::operator+(const SparseMatrix& other) const {
 
     SparseMatrix result(rows, cols, aTerms + bTerms + 1);
     int i = 1, j = 1;
-    while (i <= aTerms && j <= bTerms) {
+    while (i <= aTerms && j <= bTerms)
+    {
         int aIdx = elements[i].row * cols + elements[i].col;
         int bIdx = other.elements[j].row * cols + other.elements[j].col;
 
-        if (aIdx < bIdx) {
+        if (aIdx < bIdx)
+        {
             result.addElement(elements[i].row, elements[i].col, elements[i].data);
             i++;
-        } else if (aIdx > bIdx) {
+        }
+        else if (aIdx > bIdx)
+        {
             result.addElement(other.elements[j].row, other.elements[j].col, other.elements[j].data);
             j++;
-        } else {
+        }
+        else
+        {
             double sum = elements[i].data + other.elements[j].data;
-            if (sum != 0.0) result.addElement(elements[i].row, elements[i].col, sum);
+            if (sum != 0.0)
+                result.addElement(elements[i].row, elements[i].col, sum);
             i++;
             j++;
         }
     }
-    for (; i <= aTerms; i++) result.addElement(elements[i].row, elements[i].col, elements[i].data);
-    for (; j <= bTerms; j++) result.addElement(other.elements[j].row, other.elements[j].col, other.elements[j].data);
-    
-    result.quickSort(1, (int)result.elements[0].data);
+    for (; i <= aTerms; i++)
+        result.addElement(elements[i].row, elements[i].col, elements[i].data);
+    for (; j <= bTerms; j++)
+        result.addElement(other.elements[j].row, other.elements[j].col, other.elements[j].data);
+
+    int resTerms = (int)result.elements[0].data;
+    if (resTerms > 0)
+        std::sort(result.elements + 1, result.elements + resTerms + 1, compareElements);
     return result;
 }
 
-SparseMatrix SparseMatrix::operator-(const SparseMatrix& other) const {
+SparseMatrix SparseMatrix::operator-(const SparseMatrix &other) const
+{
     int rows = elements[0].row;
     int cols = elements[0].col;
     int aTerms = (int)elements[0].data;
@@ -112,31 +125,43 @@ SparseMatrix SparseMatrix::operator-(const SparseMatrix& other) const {
 
     SparseMatrix result(rows, cols, aTerms + bTerms + 1);
     int i = 1, j = 1;
-    while (i <= aTerms && j <= bTerms) {
+    while (i <= aTerms && j <= bTerms)
+    {
         int aIdx = elements[i].row * cols + elements[i].col;
         int bIdx = other.elements[j].row * cols + other.elements[j].col;
 
-        if (aIdx < bIdx) {
+        if (aIdx < bIdx)
+        {
             result.addElement(elements[i].row, elements[i].col, elements[i].data);
             i++;
-        } else if (aIdx > bIdx) {
+        }
+        else if (aIdx > bIdx)
+        {
             result.addElement(other.elements[j].row, other.elements[j].col, -other.elements[j].data);
             j++;
-        } else {
+        }
+        else
+        {
             double diff = elements[i].data - other.elements[j].data;
-            if (diff != 0.0) result.addElement(elements[i].row, elements[i].col, diff);
+            if (diff != 0.0)
+                result.addElement(elements[i].row, elements[i].col, diff);
             i++;
             j++;
         }
     }
-    for (; i <= aTerms; i++) result.addElement(elements[i].row, elements[i].col, elements[i].data);
-    for (; j <= bTerms; j++) result.addElement(other.elements[j].row, other.elements[j].col, -other.elements[j].data);
-    
-    result.quickSort(1, (int)result.elements[0].data);
+    for (; i <= aTerms; i++)
+        result.addElement(elements[i].row, elements[i].col, elements[i].data);
+    for (; j <= bTerms; j++)
+        result.addElement(other.elements[j].row, other.elements[j].col, -other.elements[j].data);
+
+    int resTerms = (int)result.elements[0].data;
+    if (resTerms > 0)
+        std::sort(result.elements + 1, result.elements + resTerms + 1, compareElements);
     return result;
 }
 
-SparseMatrix SparseMatrix::operator*(const SparseMatrix& other) const {
+SparseMatrix SparseMatrix::operator*(const SparseMatrix &other) const
+{
     int rows = elements[0].row;
     int other_cols = (int)other.elements[0].col;
     int aTerms = (int)elements[0].data;
@@ -145,37 +170,51 @@ SparseMatrix SparseMatrix::operator*(const SparseMatrix& other) const {
     SparseMatrix bTrans = other.transpose();
     int bTerms = (int)bTrans.elements[0].data;
 
-    for (int i = 1; i <= aTerms; ) {
+    int i = 1;
+    while (i <= aTerms)
+    {
         int r = elements[i].row;
         int rStart = i;
-        while (i <= aTerms && elements[i].row == r) i++;
+        while (i <= aTerms && elements[i].row == r)
+            i++;
         int rEnd = i;
 
-        for (int j = 1; j <= bTerms; ) {
+        int j = 1;
+        while (j <= bTerms)
+        {
             int c = bTrans.elements[j].row;
             int cStart = j;
-            while (j <= bTerms && bTrans.elements[j].row == c) j++;
+            while (j <= bTerms && bTrans.elements[j].row == c)
+                j++;
             int cEnd = j;
 
             double sum = 0.0;
             int pA = rStart, pB = cStart;
-            while (pA < rEnd && pB < cEnd) {
-                if (elements[pA].col < bTrans.elements[pB].col) pA++;
-                else if (elements[pA].col > bTrans.elements[pB].col) pB++;
-                else {
+            while (pA < rEnd && pB < cEnd)
+            {
+                if (elements[pA].col < bTrans.elements[pB].col)
+                    pA++;
+                else if (elements[pA].col > bTrans.elements[pB].col)
+                    pB++;
+                else
+                {
                     sum += elements[pA].data * bTrans.elements[pB].data;
                     pA++;
                     pB++;
                 }
             }
-            if (sum != 0.0) result.addElement(r, c, sum);
+            if (sum != 0.0)
+                result.addElement(r, c, sum);
         }
     }
-    result.quickSort(1, (int)result.elements[0].data);
+    int resTerms = (int)result.elements[0].data;
+    if (resTerms > 0)
+        std::sort(result.elements + 1, result.elements + resTerms + 1, compareElements);
     return result;
 }
 
-SparseMatrix SparseMatrix::operator/(const SparseMatrix& other) const {
+SparseMatrix SparseMatrix::operator/(const SparseMatrix &other) const
+{
     int rows = elements[0].row;
     int cols = elements[0].col;
     int aTerms = (int)elements[0].data;
@@ -183,40 +222,67 @@ SparseMatrix SparseMatrix::operator/(const SparseMatrix& other) const {
 
     SparseMatrix result(rows, cols);
     int i = 1, j = 1;
-    while (i <= aTerms && j <= bTerms) {
+    while (i <= aTerms && j <= bTerms)
+    {
         int aIdx = elements[i].row * cols + elements[i].col;
         int bIdx = other.elements[j].row * cols + other.elements[j].col;
 
-        if (aIdx < bIdx) {
+        if (aIdx < bIdx)
+        {
             i++;
-        } else if (aIdx > bIdx) {
+        }
+        else if (aIdx > bIdx)
+        {
             j++;
-        } else {
-            if (other.elements[j].data != 0.0) {
+        }
+        else
+        {
+            if (other.elements[j].data != 0.0)
+            {
                 double val = elements[i].data / other.elements[j].data;
-                if (val != 0.0) result.addElement(elements[i].row, elements[i].col, val);
+                if (val != 0.0)
+                    result.addElement(elements[i].row, elements[i].col, val);
             }
             i++;
             j++;
         }
     }
-    result.quickSort(1, (int)result.elements[0].data);
+    int resTerms = (int)result.elements[0].data;
+    if (resTerms > 0)
+        std::sort(result.elements + 1, result.elements + resTerms + 1, compareElements);
     return result;
 }
 
-SparseMatrix SparseMatrix::transpose() const {
+SparseMatrix SparseMatrix::transpose() const
+{
     SparseMatrix result(elements[0].col, elements[0].row);
     int terms = (int)elements[0].data;
-    for (int i = 1; i <= terms; i++) {
+    for (int i = 1; i <= terms; i++)
+    {
         result.addElement(elements[i].col, elements[i].row, elements[i].data);
     }
-    result.quickSort(1, (int)result.elements[0].data);
+    int resTerms = (int)result.elements[0].data;
+    if (resTerms > 0)
+        std::sort(result.elements + 1, result.elements + resTerms + 1, compareElements);
     return result;
 }
 
-void SparseMatrix::print() const {
+void SparseMatrix::print() const
+{
     int terms = (int)elements[0].data;
-    for (int i = 1; i <= terms; ++i) {
+    for (int i = 1; i <= terms; ++i)
+    {
         std::cout << "[" << elements[i].row << "][" << elements[i].col << "] = " << elements[i].data << "\n";
     }
+}
+
+double SparseMatrix::getValue(int r, int c) const
+{
+    int terms = (int)elements[0].data;
+    for (int i = 1; i <= terms; ++i)
+    {
+        if (elements[i].row == r && elements[i].col == c)
+            return elements[i].data;
+    }
+    return 0.0;
 }
