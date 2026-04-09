63b9910cc8bd9bec4a4859478bea62189f93b06f
yubin <munu220@gmail.com>
2026-04-09 22:32:26 +0900

    generateRandomData 위치 변경
    테스트 시 모든 알고리즘이 동일한 데이터를 가지고 테스트 하도록 수정

--- sort2026/cppsort/main.cpp
+++ sort2026/cppsort/main.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include <chrono>
+#include <random>
 #include "SortManager.h"
 
-void measureTime(std::string name, void (*sortFunc)(std::vector<int>&), int size) {
-    auto data = SortManager::generateRandomData(size);
-   
+std::vector<int> generateRandomData(int size)
+{
+    // 랜덤 데이터
+    std::vector<int> data(size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 1000000);
+    for (int i = 0; i < size; i++)
+        data[i] = dis(gen);
+    return data;
+}
+
+void measureTime(std::string name, void (*sortFunc)(std::vector<int> &), int size, std::vector<int> data)
+{
     auto start = std::chrono::high_resolution_clock::now();
     sortFunc(data);
     auto end = std::chrono::high_resolution_clock::now();
-   
+
     std::chrono::duration<double> diff = end - start;
     std::cout << name << " (N=" << size << "): " << diff.count() << "s" << std::endl;
 }
 
-int main() {
+int main()
+{
     int sizes[] = {100000, 125000, 150000, 175000, 200000};
-    //100000 이상의 데이터들을 순차적으로 정렬
-    for (int size : sizes) {
+    // 100000 이상의 데이터들을 순차적으로 정렬
+    for (int size : sizes)
+    {
         std::cout << "--- Testing size: " << size << " ---" << std::endl;
-        measureTime("Insertion Sort", SortManager::insertionSort, size);
-        measureTime("Selection Sort", SortManager::selectionSort, size);
-        measureTime("Bubble Sort", SortManager::bubbleSort, size);
+        auto data = generateRandomData(size);
+        measureTime("Insertion Sort", SortManager::insertionSort, size, data);
+        measureTime("Selection Sort", SortManager::selectionSort, size, data);
+        measureTime("Bubble Sort", SortManager::bubbleSort, size, data);
     }
     return 0;
 }
